f86efd280df561952dd9ffea67060d3bf4e62d22
Evyasaf <evyasafm@gmail.com>
2018-12-02 12:49:12 +0200

    Initial Commit

---
+++ MVVMRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRx
+//
+//  Created by Evyasaf on 12/2/18.
+//  Copyright © 2018 evya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
